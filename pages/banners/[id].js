36875ee7d5b76f2ffed7ded57d2785290a00d139
Raphael Isidro Rodríguez <raphaww@gmail.com>
2021-02-18 12:33:40 -0500

    - listado de banners

--- pages/banners/[id].js
+++ pages/banners/[id].js
@@ -11,10 +11,10 @@ export default function Banner({ bannerData }) {
          </Head>
          <Jumbotron fluid style={{
             ...bannerData.photos && bannerData.photos.length > 0 && {
-               'background-image': `url(//res.cloudinary.com/itermotus/f_auto,w_1280,h_600,b_black,o_70,c_fill/hotel/${bannerData.photos[0].path})`
+               backgroundImage: `url(//res.cloudinary.com/itermotus/f_auto,w_1280,h_600,b_black,o_70,c_fill/hotel/${bannerData.photos[0].path})`
             },
-            'background-position': '50%',
-            'background-size': 'cover',
+            backgroundPosition: '50%',
+            backgroundSize: 'cover',
          }}>
             <Container>
                <h1>{bannerData.title}</h1>
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,32 @@
 import Layout from '../components/layout';
 import Head from 'next/head';
+import Link from 'next/link';
+import { getAllBanners } from '../lib/banners';
 
-export default function Home() {
+export default function Home({banners}) {
   return (
     <Layout>
       <Head>
         <title>Hola mundo</title>
       </Head>
-      Hola mundo
+      <ul className="list-unstyled">
+      {banners.map(banner => (
+        <li key={banner.id}>
+          <Link href={`/banners/${banner.id}`}>
+            <a>{banner.title && banner.title !== " " ? banner.title : "[click]"}</a>
+          </Link>
+        </li>
+      ))}
+      </ul>
     </Layout>
   );
 };
+
+export async function getServerSideProps() {
+  const banners = await getAllBanners();
+  return {
+     props: {
+        banners
+     }
+  };
+}
